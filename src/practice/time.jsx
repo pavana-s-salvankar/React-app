5178d35b0feef8bded3ef025418f60d1931d8100
pavana-s-salvankar <pavanasalvankarbelle1999@gmail.com>
2021-03-26 11:43:08 +0530

    changed

--- src/practice/time.jsx
+++ src/practice/time.jsx
@@ -1,12 +1,11 @@
 function tick() {
     const element = (
       <div>
-        <h1>Hello, world!</h1>
+        <h1>Hello, world!!!!!!</h1>
         <h2>It is {new Date().toLocaleTimeString()}.</h2>
       </div>
     );
     return element;
 }
-    
   export default tick;
 
